f33428abc3b63b465f47b119656cc8b213c10968
GiorgiTseradze <giorgitseradze@redberry.ge>
2022-12-15 17:38:54 +0400

    feat: fix reset password rule and view

--- src/components/Auth/NewPassword.vue
+++ src/components/Auth/NewPassword.vue
@@ -16,18 +16,42 @@
                         <div class="w-[22.5rem]">
                             <section class="flex text-white">{{ $t("auth.password") }}<p class="text-red-500 ml-1"> *</p></section>
                         </div>
-                        <div class="flex w-[22.5rem] justify-center">
-                            <Field name="password" class="bg-[#CED4DA] w-full h-[2.3rem] rounded px-3 mt-2 placeholder-text-xs outline-none" :placeholder="$t('auth.at_least_x_lower_case_characters')" />
-                        </div>
+                        <div class="flex w-[22.5rem]">
+                        <Field name="password" rules="required|min:8|max:15" v-slot="{ field, meta }"
+                         :placeholder="$t('auth.password')">
+                            <input class="relative bg-[#CED4DA] w-full h-[2.3rem] rounded px-3 mt-2 outline-none" v-bind="field" :type="showPassword ? 'text' : 'password'"
+                                :class="[!meta.valid && meta.touched ? 'border-red-500 border-2' 
+                                : '', meta.valid && meta.touched ? 'border-green-500 border-2' : '']" />
+                            <img :class="meta.valid && meta.touched ? 'block' : 'hidden'" class="absolute ml-80 mt-3 cursor-pointer p-1" 
+                            src="@/assets/valid.svg" alt="valid-icon" />
+                        </Field>
+                        <img v-if="showPassword" @click="handleShow" class="absolute ml-[18.7rem] mt-[0.9rem] cursor-pointer py-1 px-1" 
+                        src="@/assets/eye.svg" alt="eye-icon" />
+                        <img v-if="!showPassword" @click="handleShow" class="absolute ml-[18.7rem] mt-[0.9rem] cursor-pointer py-1 px-1" 
+                        src="@/assets/closed-eye.svg" alt="closed-eye-icon" />
+                        <ErrorMessage class="absolute mt-[2.8rem] py-1 text-sm text-[#F15524]" name="password" />
+                    </div>
                     </div>
 
                     <div class="flex flex-col items-center w-full mt-6">
                         <div class="w-[22.5rem]">
                             <section class="flex text-white">{{ $t("auth.password_confirmation") }}<p class="text-red-500 ml-1"> *</p></section>
                         </div>
-                        <div class="flex w-[22.5rem] justify-center">
-                            <Field name="password_confirmation" class="bg-[#CED4DA] w-full h-[2.3rem] rounded px-3 mt-2 outline-none" :placeholder="$t('auth.password_confirmation')" />
-                        </div>
+                        <div class="flex flex-col w-[22.5rem] relative pb-2">
+                        <Field name="password_confirmation" rules="required|min:8|max:15" v-slot="{ field, meta }"
+                        :placeholder="$t('auth.password_confirmation')">
+                            <input class="relative bg-[#CED4DA] w-full h-[2.3rem] rounded px-3 mt-2 outline-none" v-bind="field" :type="showPasswordConfirm ? 'text' : 'password'"
+                                :class="[!meta.valid && meta.touched ? 'border-red-500 border-2' 
+                                : '', meta.valid && meta.touched ? 'border-green-500 border-2' : '']" />
+                            <img :class="meta.valid && meta.touched ? 'block' : 'hidden'" class="absolute ml-80 mt-3 cursor-pointer p-1" src="@/assets/valid.svg" />
+                        </Field>
+                        <img v-if="showPasswordConfirm" @click="handleShowConfirm" class="absolute ml-[18.7rem] mt-[0.9rem] cursor-pointer py-1 px-1" 
+                        src="@/assets/eye.svg" alt="eye-icon" />
+                        <img v-if="!showPasswordConfirm" @click="handleShowConfirm" class="absolute ml-[18.7rem] mt-[0.9rem] cursor-pointer py-1 px-1" 
+                        src="@/assets/closed-eye.svg" alt="closed-eye-icon" />
+                        <ErrorMessage class="absolute mt-[3rem] py-1 text-sm text-[#F15524]" name="password_confirmation" />
+                    </div>
+
                     </div>
         
                     <div class="flex flex-col items-center w-full mt-5">
@@ -49,14 +73,27 @@
 </template>
 
 <script setup>
-import { Field, Form } from 'vee-validate';
+import { Field, ErrorMessage, Form } from 'vee-validate';
 import axiosInstance from "@/config/axios/index.js";
 import { useRoute, useRouter } from 'vue-router';
 import { onBeforeMount, ref } from 'vue';
+import i18n from '@/i18n/index.js'
 
 const router = useRouter()
 const token = ref();
 const email = ref('');
+const showPassword = ref(false);
+const showPasswordConfirm = ref(false);
+
+const handleShow = () => {
+    showPassword.value = !showPassword.value
+}
+
+const handleShowConfirm = () => {
+    showPasswordConfirm.value = !showPasswordConfirm.value
+}
+
+
 onBeforeMount(()=>{
     if(useRoute().query.reset_password_token){
         token.value = useRoute().query.reset_password_token;
@@ -68,7 +105,7 @@ onBeforeMount(()=>{
 })
 
 
-const handleSubmit = (values) => {
+const handleSubmit = (values, actions) => {
     axiosInstance
         .post("reset-password", {
             email: email.value,
@@ -77,10 +114,21 @@ const handleSubmit = (values) => {
             password_confirmation: values.password_confirmation,
         })
         .then(() => {
-          router.push({ name: "login" });
+          router.push({ name: "passwordSuccess" });
         })
         .catch((error) => {
-          console.log(error);
+        const errors = error.response.data.errors;
+          for(const err in errors){
+            if (i18n.global.locale == "en") {
+                if (err === "password") {
+                actions.setFieldError("password", "Password doesn't match");
+                }
+            } else {
+                if (err === "password") {
+                actions.setFieldError("password", "პაროლები არ ემთხვევა");
+                }
+            }
+        }
         });
 }
 </script>
